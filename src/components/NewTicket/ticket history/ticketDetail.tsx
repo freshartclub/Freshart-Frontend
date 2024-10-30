24c21062f72958d56f92a063ae93810bc04ea08e
freshartclub <devops@freshartclub.com>
2024-10-30 17:20:27 +0530

    added chnages

--- src/components/NewTicket/ticket history/ticketDetail.tsx
+++ src/components/NewTicket/ticket history/ticketDetail.tsx
@@ -46,7 +46,7 @@ const SingleTicket = () => {
         <div className="flex items-center gap-2">
           <span className="w-2.5 h-2.5 bg-[#F8A53499] rounded-full"></span>
           <span className="font-semibold text-gray-800">
-            {ticket.data?.ticketId}
+            {ticket?.data?.ticketId}
           </span>
           <span className="text-[#84818A]">({ticket.data?.ticketType})</span>
         </div>
@@ -55,7 +55,7 @@ const SingleTicket = () => {
           <span className="w-2.5 h-2.5 bg-[#F8A53499] rounded-full"></span>
           <span className="text-[#84818A]">Ongoing</span>
           <div className="text-gray-400 text-sm">
-            {dayjs(ticket.data?.createdAt).format("MMMM D, YYYY")}
+            {dayjs(ticket?.data?.createdAt).format("MMMM D, YYYY")}
           </div>
         </div>
       </div>
@@ -67,46 +67,48 @@ const SingleTicket = () => {
 
       <div className="mt-10">
         <h1 className="w-[406px] h-[23px] top-2 font-montserrat text-[18px] font-semibold leading-[14px] text-left text-[#2E2C34]">
-          {ticket.data?.subject}
+          {ticket?.data?.subject}
         </h1>
         <p className="text-[#84818A] font-montserrat text-sm font-medium leading-[17.07px] text-left mb-6">
-          {ticket.data?.message}
+          {ticket?.data?.message}
         </p>
       </div>
 
-      {ticket.reply.map((item, i) => (
-        <div className="bg-[#919EAB29] p-4">
-          <div className="flex justify-between">
-            <h1 className="font-montserrat text-[18px] font-semibold leading-[14px] text-left text-[#2E2C34] mb-2 ml-4">
-              Replied By Admin
-            </h1>
-            <div>
-              <span
-                className={`ml-3 font-semibold rounded-lg px-2 ${
-                  item.status === "In progress"
-                    ? "bg-[#F8A53499]"
-                    : "bg-green-500"
-                }`}
-              >
-                {item.status}
-              </span>
-              <span className="ml-3  font-semibold">{item.ticketType}</span>
-              <span className="ml-3  font-semibold">
-                {dayjs(item?.createdAt).format("MMMM D, YYYY")}
+      {ticket?.reply &&
+        ticket?.reply?.length &&
+        ticket?.reply.map((item, i) => (
+          <div className="bg-[#919EAB29] p-4">
+            <div className="flex justify-between">
+              <h1 className="font-montserrat text-[18px] font-semibold leading-[14px] text-left text-[#2E2C34] mb-2 ml-4">
+                Replied By Admin
+              </h1>
+              <div>
+                <span
+                  className={`ml-3 font-semibold rounded-lg px-2 ${
+                    item.status === "In progress"
+                      ? "bg-[#F8A53499]"
+                      : "bg-green-500"
+                  }`}
+                >
+                  {item.status}
+                </span>
+                <span className="ml-3  font-semibold">{item.ticketType}</span>
+                <span className="ml-3  font-semibold">
+                  {dayjs(item?.createdAt).format("MMMM D, YYYY")}
+                </span>
+              </div>
+            </div>
+            <div className="flex items-center mb-4 ml-4">
+              <span className="ml-3 text-[#84818A] font-semibold">
+                {item.userType}
               </span>
             </div>
-          </div>
-          <div className="flex items-center mb-4 ml-4">
-            <span className="ml-3 text-[#84818A] font-semibold">
-              {item.userType}
-            </span>
-          </div>
 
-          <p className="text-[#84818A] font-montserrat text-sm font-medium leading-[17.07px] text-left mb-6 ml-6 pr-4">
-            {item.message}
-          </p>
-        </div>
-      ))}
+            <p className="text-[#84818A] font-montserrat text-sm font-medium leading-[17.07px] text-left mb-6 ml-6 pr-4">
+              {item.message}
+            </p>
+          </div>
+        ))}
 
       <div className="p-4">
         <h2 className="font-montserrat text-lg font-semibold mb-2 ml-2 mt-4">
