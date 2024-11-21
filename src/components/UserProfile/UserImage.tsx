21bb63da1dc153823f0a77208b6c896e7d860015
freshartclub <devops@freshartclub.com>
2024-11-21 17:56:33 +0530

    chnages done

--- src/components/UserProfile/UserImage.tsx
+++ src/components/UserProfile/UserImage.tsx
@@ -4,7 +4,7 @@ const UserImage = ({ user, url }) => {
   return (
     <div className="lg:w-[20%] md:w-[25%] w-full">
       <img
-        src={`${url}/users/${user.profile.mainImage}`}
+        src={`${url}/users/${user?.profile?.mainImage}`}
         alt="user profile"
         className="w-full sm:w-auto"
       />
